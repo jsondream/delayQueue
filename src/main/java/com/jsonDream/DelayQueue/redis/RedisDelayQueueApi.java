dfe03788a03826622556b81269d89793ed8b1b77
wangguangdong <wangguangdong@kingdowin.com>
2016-04-08 17:07:07 +0800

    提交

--- src/main/java/com/jsonDream/DelayQueue/redis/RedisDelayQueueApi.java
+++ src/main/java/com/jsonDream/DelayQueue/redis/RedisDelayQueueApi.java
@@ -11,7 +11,7 @@ import redis.clients.jedis.Tuple;
 
 /**
  * <p>
- * redis的authKey接口
+ * redis的延迟队列
  * </p>
  *
  * @author wangguangdong
@@ -20,19 +20,44 @@ import redis.clients.jedis.Tuple;
  */
 
 public class RedisDelayQueueApi extends BaseRedisApi {
+
+	/**
+	 * 用单例的操作队列
+	 */
+	private RedisDelayQueueApi(){}
 	
+	private static class LazyHolder{
+		private static RedisDelayQueueApi instance = new RedisDelayQueueApi();
+	}
+	
+	public static RedisDelayQueueApi getInstance(){
+		return LazyHolder.instance;
+	}
+	
+	/**
+	 * 借鉴delayQueue的锁机制
+	 */
 	private final transient ReentrantLock lock = new ReentrantLock();
 
-	 /**
-     * Condition signalled when a newer element becomes available
-     * at the head of the queue or a new thread may need to
-     * become leader.
-     */
-    private final Condition available = lock.newCondition();
-    
-    
+	/**
+	 * Condition signalled when a newer element becomes available at the head of
+	 * the queue or a new thread may need to become leader.
+	 */
+	private final Condition available = lock.newCondition();
+
+	/**
+	 * 参考了delayQueue的内部结构
+	 */
+	private Thread leader = null;
+
+	/**
+	 * 存储延迟任务的队列
+	 */
 	private static final String delayedQueueKeyName = "DelayedQueueSet";
 
+	/**
+	 * 真正需要执行的队列
+	 */
 	private static final String executeQueueKeyName = "DelayedQueueExecuteSet";
 
 	/**
@@ -52,29 +77,56 @@ public class RedisDelayQueueApi extends BaseRedisApi {
 				redis.rpush(executeQueueKeyName, businessObjectString);
 			}
 		});
-
 	}
 
-	public void poll_queue() {
-		while (true) {
-			Set<Tuple> set = RedisClient.domain(redis -> redis.zrangeWithScores(delayedQueueKeyName, 0, 0));
-			if (set.isEmpty()) {
-				// TODO:阻塞当前线程
-			} else {
-				Tuple tuple = set.iterator().next();
-				// 获取业务字段
-				String elementValue = tuple.getElement();
-				// 时间
-				double sys = tuple.getScore();
+	public void take_queue() throws InterruptedException {
 
+		final ReentrantLock lock = this.lock;
+		lock.lockInterruptibly();
+		try {
+			for (;;) {
+				// 查询队首
+				Tuple tuple = peek();
+				if (tuple == null) {
+					// 阻塞当前线程
+					available.await();
+				}
+				// 获取业务对象信息
+				String elementValue = peek().getElement();
+				// 执行的时间
+				double execTime = tuple.getScore();
+				// 当前系统时间
+				long concurrentTime = System.currentTimeMillis();
+				if (execTime - concurrentTime <= 0) {
+					// TODO:执行真正的队列任务
+				}
 			}
+		} finally {
+			if (leader == null && peek() != null)
+				available.signal();
+			lock.unlock();
 		}
 	}
 
-	public static void main(String[] a) {
+	/**
+	 * 队首查询
+	 * 
+	 * @return
+	 * @throws InterruptedException
+	 */
+	private Tuple peek() throws InterruptedException {
+		Set<Tuple> set = RedisClient.domain(redis -> redis.zrangeWithScores(delayedQueueKeyName, 0, 0));
+		if (set.isEmpty()) {
+			return null;
+		} else {
+			return set.iterator().next();
+		}
+	}
+
+	public static void main(String[] a) throws Exception {
 		RedisDelayQueueApi redisDelayQueueApi = new RedisDelayQueueApi();
 		redisDelayQueueApi.execute_later("sss", 60l * 1000l * 60l * 24l * 30l * 3l);
-		redisDelayQueueApi.poll_queue();
+		redisDelayQueueApi.take_queue();
 	}
 
 }
